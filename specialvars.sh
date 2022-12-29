7a95de2860d42d41e9cedca2847a0a323796f2f9
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-29 23:42:59 +0530

    updated

--- specialvars.sh
+++ specialvars.sh
@@ -2,3 +2,5 @@
 echo $0 #prints script name
 echo $? #prints exit code of last command
 echo " $1..9 tells value of argument from ist to 9th"
+echo $* #prints all variables used in script
+echo $# #prints no of variables used in script
