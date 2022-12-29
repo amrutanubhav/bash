a45373a7c00ad86dfb61c307ca6669893cba78b2
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-29 22:58:26 +0530

    updated

---
+++ colour.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#code to display hello world
+#escape sequence
+#newline \n
+#tabspace \t
+# -e is to enable escape sequen
+####################################################
+
+#color coding for bash
+echo "\e[32m i am printing green colour \e[0m"
