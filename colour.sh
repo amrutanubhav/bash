2e65a806ddecc08d6076fb0e284be34f867d6247
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-29 23:04:39 +0530

    updated

--- colour.sh
+++ colour.sh
@@ -6,8 +6,11 @@
 # -e is to enable escape sequen
 ####################################################
 
-#color coding for bash
+#color coding for bash syntax \e[codem....\e[0m]
 echo -e "\e[32m i am printing green colour \e[0m"
 echo -e "\e[31m i am printing red colour \e[0m"
 echo -e "\e[33m i am printing yellow colour \e[0m"
 echo -e "\e[34m i am printing blue colour \e[0m"
+
+#print background color "\e[41;33m ..... [0m"
+echo -e "\e[41;33m this is test for background color [0m"
