cbe81ef1a83aea519ac4e7e03692897538df6338
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-29 23:12:46 +0530

    updated

---
+++ variables.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+#what is a variable in prog?
+#variable hold data which can keep on changing as per situation
+
+a=10
+b=20
+
+echo a
+echo $a
+echo ${a}
+
+#string values
+d="amrut is a man"
+echo "$d"
+echo $e
