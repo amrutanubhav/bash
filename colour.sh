75c0fd37e1793a19b9580578f026736dc47a2cfe
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-29 23:00:24 +0530

    updated

--- colour.sh
+++ colour.sh
@@ -8,3 +8,6 @@
 
 #color coding for bash
 echo -e "\e[32m i am printing green colour \e[0m"
+echo -e "\e[31m i am printing red colour \e[0m"
+echo -e "\e[33m i am printing yellow colour \e[0m"
+echo -e "\e[34m i am printing blue colour \e[0m"
