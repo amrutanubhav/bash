68ae05e61ac8030f3815e6d8955f5187fc510b8b
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-30 12:31:48 +0530

    updated

--- functions.sh
+++ functions.sh
@@ -2,10 +2,15 @@
 
 stat() {
 
-echo "number of open sessions is : \e[41;32m $(who | wc -l) \e[0m"
-echo "total number of users logged in is:$(uptime | awk -F, '{print $2}')"
+echo "number of open sessions is : $(\e[41;32m $(who | wc -l) \e[0m)"
+echo "total number of users loggedin is: $(uptime | awk -F, '{print $2}')"
 echo "date is : $(date +%F)"
 
 }
 
 stat
+
+
+# dajdandnakd asamdasn adsasm
+# skdbakadsj
+# ksjndal
