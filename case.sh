aa5051799cdd30cae559f6f8c1e04af2788cf1ff
Amrut Anubhav <anubhavamrut@gmail.com>
2022-12-31 09:26:30 +0530

    updated

--- case.sh
+++ case.sh
@@ -4,14 +4,18 @@ read action
 case $action in
      start)
         echo "we are starting"
+        exit 0
         ;;
      restart)
         echo "we are restarting"
+        exit 0
         ;;
      stop)
         echo "we are stopping"
+        exit 0
         ;;
      *)
        echo -e "\e[32m please enter valid option \e[0m"
+       exit 1
        ;;
 esac  
