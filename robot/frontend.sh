0a5712c43a4cc14577afb5e6def555e0cc78773d
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-01 21:10:57 +0530

    updated

--- robot/frontend.sh
+++ robot/frontend.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 component=frontend
-logfile=/tmp/frontend.log
+logfile=/tmp/$component.log
 stat(){
 
 if [ $1 -eq 0 ]; then
--- robot/mongodb.sh
+++ robot/mongodb.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
-echo "im mongodb"
+
+set -e
+component=mongodb
+logfile=/tmp/$component.log
+stat(){
+
+if [ $1 -eq 0 ]; then
+  echo -e "\e[32m success \e[0m"
+else
+  echo -e "\e[31m failure \e[0m"
+fi
+
+}
+
+ID=$(id -u)
+
+if [ $ID -ne 0 ]; then
+
+echo -e "\e[31m Please use root account or sudo privilege to proceed further \e[0m"
+exit 1
+
+fi
+
+echo -n " Downloading $component repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo &>> $logfile
+stat $?
+
+echo -n " Installing $component repo"
+yum install -y mongodb-org &>> $logfile
+systemctl enable mongod &>> $logfile
+systemctl start mongod &>> $logfile
+stat $?
+
+echo -n "Downloading the $component schema: "
+curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip" &>> $logfile
+stat $?
+
+echo -n "Extracting the $component schema file"
+ cd /tmp
+ unzip mongodb.zip &>> $logfile
+ stat $?
+
+ echo -n "Injecting the schema : "
+ cd mongodb-main 
+ mongo < catalogue.js &>> $logfile
+ mongo < users.js &>> $logfile
+ stat $?
