cb3bb53a97e118b8a5086af84827cd5844b22855
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-02 14:21:39 +0530

    updated

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -27,21 +27,18 @@ echo -n "downloading the $component"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" &>> ${logfile}
 stat $?
 
-echo -n "cleanup and Extracting $component"
+echo -n "cleanup and Extracting $component: "
 rm -rf /home/$user/$component
 cd /home/$user
 unzip -o /tmp/$component.zip &>> ${logfile}
 stat $?
 
-echo -n "Changing the ownership to $user"
-
+echo -n "Changing the ownership to $user: "
 mv /home/$user/$component-main $component
 chown -R $user:$user /home/$user/$component
-
 stat $?
 
-
-
-
-#cd /home/roboshop/catalogue
-# $ npm install
+echo -n "Generate artifacts for $component: "
+cd /home/$user/$component/
+npm install &>> ${logfile}
+stat $?
