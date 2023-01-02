2d48e408b766f928c087de849f3dbe14ede28f07
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-02 14:13:24 +0530

    updated

--- robot/catalogue.sh
+++ robot/catalogue.sh
@@ -27,7 +27,8 @@ echo -n "downloading the $component"
 curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip" &>> ${logfile}
 stat $?
 
-echo -n "Extracting $component"
+echo -n "cleanup and Extracting $component"
+rm -rf /home/$user/$component
 cd /home/$user
 unzip -o /tmp/$component.zip &>> ${logfile}
 stat $?
@@ -35,7 +36,7 @@ stat $?
 echo -n "Changing the ownership to $user"
 
 mv /home/$user/$component-main $component
-chown $user:$user /home/$user/$component
+chown -R $user:$user /home/$user/$component
 
 stat $?
 
