6ed6cca62b367df4864f3ff9fda36c452c24ba9e
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-02 23:03:00 +0530

    updated

--- robot/mysql.sh
+++ robot/mysql.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
-echo "im msql"
+component=mysql
+
+source robot/common.sh
+
+echo -n " Setup $component repository:  "
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo &>> ${logfile}
+stat $?
+
+echo -n "Install $component : "
+yum install mysql-community-server -y &>> ${logfile}
+stat $?
+
+echo -n "Enable and restart $component : "
+systemctl enable mysqld 
+systemctl start mysqld
+stat $?
+
