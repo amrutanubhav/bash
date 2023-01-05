7c9e5bb49c818ca79f887aac8ccdfc91360f5fdc
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-05 23:10:52 +0530

    updated

--- robot/rabbitmq.sh
+++ robot/rabbitmq.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-echo "im rabbitmq"
+component=rabbitmq
+source robot/common.sh
+
+echo -n "Install erlang dependancy for $component: "
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash  &>> ${logfile}
+stat $?
+
+echo -n "Setup YUM repositories for $component: "
+curl -s https://packagecloud.io/install/repositories/$component/$component-server/script.rpm.sh | sudo bash &>> ${logfile}
+stat $?
+
+echo -n "install $component: "
+yum install $component-server -y &>> ${logfile}
+stat $?
+
+echo -n "enable and start $component: "
+systemctl enable rabbitmq-server 
+systemctl start rabbitmq-server
+systemctl status rabbitmq-server -l
+stat $?
+
+echo -n "create application use for $component: "
+
+rabbitmqctl add_user roboshop roboshop123  
+rabbitmqctl set_user_tags roboshop administrator
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+
+stat $?
+
