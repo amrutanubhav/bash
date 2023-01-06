455e39bbdba934a347c3e870876cee792152c8b8
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-06 10:43:33 +0530

    updated

--- robot/payment.sh
+++ robot/payment.sh
@@ -10,7 +10,7 @@ user_id=$(id $user -u)
 grp_id=$(id $user -g)
 
 sed -i -e "/^uid/ c uid=${user_id}" /home/$user/$component/$component.ini
-sed -i -e "/^gid/ c uid=${grp_id}" /home/$user/$component/$component.ini
+sed -i -e "/^gid/ c gid=${grp_id}" /home/$user/$component/$component.ini
 
 stat $?
 
