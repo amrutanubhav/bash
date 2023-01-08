0a8d4874d3640f6cb1c479d4b755560e4d082981
Amrut Anubhav <anubhavamrut@gmail.com>
2023-01-08 20:33:41 +0530

    updated

--- robot/launch-ec2.sh
+++ robot/launch-ec2.sh
@@ -4,8 +4,8 @@ component=$1
 ami_id=${aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" --region us-east-1 | jq .Images[].ImageId | sed -e 's/"//g'}
 sec_grp_id=${aws ec2 describe-security-groups --filters Name=group-name,Values=Batch52-securitygroup --region us-east-1 | jq .SecurityGroups[].GroupId | sed -e 's/"//g'}
 
-echo -e "Fetching ami id : \e[31m ${ami_id} \e[0m"
-echo -e "Fetching sec grp id: \e[31m ${sec_grp_id} \e[0m"
+echo -e "Fetching ami id : ${ami_id}"
+echo -e "Fetching sec grp id:  ${sec_grp_id} "
 
 echo "launch instance using ami id and sec grp id "
 
